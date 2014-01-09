2dae54173494058bfe95625994f236a4ebe907d7
user <user@lubuntu.(none)>
2014-01-08 19:17:33 -0500

    Updated some CSS3 animations

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
 window.onload = function() {
 	//Fade In
 	document.documentElement.className += ' loaded';
-}
+
+	var
+		frontCover = document.getElementByClassName('front');
+
+	frontCover.onclick = albumActive;
+
+	var albumAction = function() {
+		frontCover.classList.add('spin-album');
+	};
+
+};
